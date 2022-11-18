9913c91a44be7fb2864c403dbf208539ae5b581d
Arsh14ali <arshad.ali@truminds.com>
2022-11-18 14:11:46 +0530

    Added Get request at endpoin /user/users to see all the user

--- src/main/java/com/metro/metromilan/controller/userRegistrationController.java
+++ src/main/java/com/metro/metromilan/controller/userRegistrationController.java
@@ -5,13 +5,13 @@ import com.metro.metromilan.services.IuserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
+@RequestMapping("/user")
 public class userRegistrationController {
 
     @Autowired
@@ -25,5 +25,11 @@ public class userRegistrationController {
         LOGGER.info("Inside the User Registration Controller Class");
         System.out.println("The Custom Object value is : " + userClassRegistration);
         return iuserService.saveUser(userClassRegistration);
+    }
+
+    @GetMapping("/users")
+    private List<UserClassRegistration> getAllUsersList(){
+        LOGGER.info("Inside the getAllUsers Controller And fetching all the user data");
+        return iuserService.getAllUsersList();
     }
 }
